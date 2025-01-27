87a29f85e335f740355ef37340f0606604074c0b
Bhuvaneshwari V <muskie472@gmail.com>
2025-01-27 16:17:18 +0530

    Add files via upload

---
+++ client-side-routing/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HeroSection from './components/HeroSection'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
